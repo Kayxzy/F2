3a9615064336667e2bd5e94f7eaf485a0693ed4a
Kayxzy <106707683+Kayxzy@users.noreply.github.com>
2024-03-10 00:09:01 +0700

    Update sql.py

--- database/sql.py
+++ database/sql.py
@@ -6,8 +6,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from config import DB_URI
 
-
-    def start() -> scoped_session:
+def start() -> scoped_session:
         engine = create_engine(DB_URL, client_encoding="utf8")
         BASE.metadata.bind = engine
         BASE.metadata.create_all(engine)
