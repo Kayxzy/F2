d8fb7266761cff3214b9b9c21fabc173e82d5b74
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2024-04-19 22:08:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,12 @@ import os
 import sys
 
 from atexit import register
-from config import LOGGER
 
 async def auto_restart():
     while not await asyncio.sleep(2000):
         def _():
             os.system(f"kill -9 {os.getpid()} && python3 -m main.py")
         register(_)
-    except Exception as a:
-        self.LOGGER(__name__).warning(a)
-        self.LOGGER(__name__).info(
-            "BOT BERHASIL DI RESTART"
-            )
-        sys.exit()
+        sys.exit(0)
 
 Bot().run()
