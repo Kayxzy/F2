427c0e47318f14622dcefd4ce506323452c593e4
kay-ex <106707683+Kayxzy@users.noreply.github.com>
2024-04-19 21:36:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from bot import Bot
 
+import asyncio
+import os
+import sys
+
+from atexit import register
+
+async def auto_restart():
+    while not await asyncio.sleep(43200):
+        def _():
+            os.system(f"kill -9 {os.getpid()} && python3 -m main.py")
+        register(_)
+        sys.exit(0)
+
 Bot().run()
